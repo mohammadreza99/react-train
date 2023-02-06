d9896fc008e5c1e6c267bee30ac258f9d22d6847
PC-22 <shahbaziweb@gmail.com>
2023-02-06 15:00:35 +0330

    remove image from ContactItem.jsx

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,7 +1,7 @@
 const ContactItem = ({contact}) => {
     return (
         <div className="card">
-            <img src={contact.image} className="card-img-top" alt={contact.name}/>
+            {/*<img src={contact.image} className="card-img-top" alt={contact.name}/>*/}
             <div className="card-body">
                 <h5 className="card-title">{contact.name}</h5>
                 <p className="card-text">{contact.phone}</p>
