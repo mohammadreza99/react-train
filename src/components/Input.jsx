33ab5ae376a99b13f76dfc62ef8ace533883a3b7
PC-22 <shahbaziweb@gmail.com>
2023-02-06 15:02:14 +0330

    remove error section from Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,8 @@
-export const Input = ({onChange,label,name,error}) => {
+export const Input = ({onChange, label, name}) => {
     return (
         <div className="mb-3">
-            <label htmlFor={name} className="form-label">{label}</label>
-            <input className="form-control" name={name} id={name}
-                   onChange={onChange}/>
-            {error && (
-                <h1 style={{backgroundColor:"crimson",color:"palegreen"}}>
-                    {error?.[name]}
-                </h1>
-            )}
+            {label && <label htmlFor={name} className="form-label">{label}</label>}
+            <input className="form-control" name={name} id={name} onChange={onChange}/>
         </div>
     )
 }
